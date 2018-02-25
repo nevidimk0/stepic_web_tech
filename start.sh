bdf5d602b37ff8ddf6ed83bc5a23ed7a1dc3acd9
nevidimk0 <nevidimk0@inbox.ru>
2018-02-25 16:31:14 +0300

    Added new commands

--- start.sh
+++ start.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 apt-get update
 apt-get install -y nginx
+mkdir -p /home/box/web
+mkdir -p /home/box/web/public
+mkdir -p /home/box/web/public/img
+mkdir -p /home/box/web/public/css
+mkdir -p /home/box/web/public/js
+mkdir -p /home/box/web/uploads
+mkdir -p /home/box/web/etc
+cp nginx.conf /home/box/web/etc/nginx.conf
+ln /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/stepic.conf
+service nginx restart
