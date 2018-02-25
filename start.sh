86eac31fb023fa45c8994a1bf3752ae209f16da8
nevidimk0 <nevidimk0@inbox.ru>
2018-02-25 17:42:52 +0300

    modified

--- start.sh
+++ start.sh
@@ -10,4 +10,6 @@ mkdir -p /home/box/web/uploads
 mkdir -p /home/box/web/etc
 cp nginx.conf /home/box/web/etc/nginx.conf
 ln /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/stepic.conf
+rm -rf /etc/nginx/sites-available/default
+rm -rf /etc/nginx/sites-enabled/default
 service nginx restart
